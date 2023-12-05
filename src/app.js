629db39f956c6f3a6c20341dd2fecff3dd066c3d
Anonymous Soldier <a96111085154015@gmail.com>
2023-12-05 14:17:01 +0330

    Received products information from json file.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 "use strict";
+
+import fetchData from "./asset/utilities/httpReq.js";
+
+async function render() {
+  const productsData = await fetchData();
+  console.log(productsData);
+}
+
+document.addEventListener("DOMContentLoaded", render);
